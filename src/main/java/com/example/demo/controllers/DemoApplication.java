3efb8aa014b6ffeb5eddd93c62fc6c36906c3ff1
josegulmanelli <jgulmanelli@grupomercel.com>
2023-01-13 10:28:59 -0300

    Starting commit

---
+++ src/main/java/com/example/demo/controllers/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	private static final String template = "Hello, %s!";
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format(template, name);
+	}
+}
